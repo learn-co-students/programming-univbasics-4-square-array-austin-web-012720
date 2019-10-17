f2572fee0bd8e6228f7835514f18076ca76e79eb
Rick Glascock <rixong@gmail.com>
2019-10-17 14:08:12 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,15 @@
+# def square_array(array)
+#   count = 0
+#   while array[count] do
+#     array[count] = array[count]**2
+#     count += 1
+#   end
+#   return array
+# end
+
 def square_array(array)
   count = 0
-  while array[count] do
+  while count < array.length do
     array[count] = array[count]**2
     count += 1
   end
